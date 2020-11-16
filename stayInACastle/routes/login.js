25ca8afce3eebda999e380918700068989d754a9
Issa Nikole Gruber <issanikolegruber@Issas-MacBook-Pro.local>
2020-11-16 14:03:21 +0100

    took out auth folder

--- stayInACastle/routes/login.js
+++ stayInACastle/routes/login.js
@@ -5,14 +5,14 @@ const bcryptjs = require("bcrypt");
 const User = require("../models/User.model");
 const { resource } = require("../app");
 
-router.get("/login", (req, res) => {res.render("auth/login", { errorMessage: req.flash('error')})
+router.get("/login", (req, res) => {res.render("/login", { errorMessage: req.flash('error')})
     });
 
 router.post("/login", (req, res, next) => {
     const { email, passwordHash } = req.body
 
     if (email === '' || password === '') {
-        res.render('auth/login', {
+        res.render('/login', {
             errorMessage: 'Please enter both, email and password to login'
         })
         return;
@@ -21,14 +21,14 @@ router.post("/login", (req, res, next) => {
     User.findOne({ email })
         .then ( user => {
             if (!user) {
-                res.render('auth/login', { errorMessage: 'Email is not registered. Try another email.'});
+                res.render('/login', { errorMessage: 'Email is not registered. Try another email.'});
                 return;
             } else if (bcryptjs.compareSync(passwordHash, user.passwordHash)) {
                 req.session.currentUser = user;
                 console.log(user)
-                res.redirect('/user-profile');
+                res.redirect('');
             } else {
-                res.render('auth/login', { errorMessage: 'Incorrect password.'});
+                res.render('/login', { errorMessage: 'Incorrect password.'});
             }
         })
         .catch(error => next(error));
