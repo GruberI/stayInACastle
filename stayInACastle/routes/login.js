d26a7f6126ed72bc613b25c832c92f6e90119f7c
Issa Nikole Gruber <issanikolegruber@Issas-MacBook-Pro.local>
2020-11-16 14:32:41 +0100

    seeded db

--- stayInACastle/routes/login.js
+++ stayInACastle/routes/login.js
@@ -1,37 +1,36 @@
-const express = require('express');
-const router  = express.Router();
-const bcryptjs = require("bcrypt");
+// const { Router } = require("express");
+// const routerLogin = new Router;
+// const bcryptjs = require("bcrypt");
 
-const User = require("../models/User.model");
-const { resource } = require("../app");
+// const User = require("../models/User.model");
 
-router.get("/login", (req, res) => {res.render("/login", { errorMessage: req.flash('error')})
-    });
+// router.get("/login", (req, res, next) => 
+// res.render("login"));
 
-router.post("/login", (req, res, next) => {
-    const { email, passwordHash } = req.body
+// router.post("/login", (req, res, next) => {
+//     const { email, passwordHash } = req.body
 
-    if (email === '' || password === '') {
-        res.render('/login', {
-            errorMessage: 'Please enter both, email and password to login'
-        })
-        return;
-    } 
+//     if (email === '' || password === '') {
+//         res.render('/login', {
+//             errorMessage: 'Please enter both, email and password to login'
+//         })
+//         return;
+//     } 
 
-    User.findOne({ email })
-        .then ( user => {
-            if (!user) {
-                res.render('/login', { errorMessage: 'Email is not registered. Try another email.'});
-                return;
-            } else if (bcryptjs.compareSync(passwordHash, user.passwordHash)) {
-                req.session.currentUser = user;
-                console.log(user)
-                res.redirect('');
-            } else {
-                res.render('/login', { errorMessage: 'Incorrect password.'});
-            }
-        })
-        .catch(error => next(error));
-})
+//     User.findOne({ email })
+//         .then ( user => {
+//             if (!user) {
+//                 res.render('/login', { errorMessage: 'Email is not registered. Try another email.'});
+//                 return;
+//             } else if (bcryptjs.compareSync(passwordHash, user.passwordHash)) {
+//                 req.session.currentUser = user;
+//                 console.log(user)
+//                 res.redirect('');
+//             } else {
+//                 res.render('/login', { errorMessage: 'Incorrect password.'});
+//             }
+//         })
+//         .catch(error => next(error));
+// })
 
-module.exports = router;
+// module.exports = routerLogin;
