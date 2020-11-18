672f7f5d93c50ebf0e49ee534522523fb9532666
Issa Nikole Gruber <issanikolegruber@Issas-MacBook-Pro.local>
2020-11-18 16:19:10 +0100

    changes

--- stayInACastle/app.js
+++ stayInACastle/app.js
@@ -46,19 +46,16 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+    
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Stay in a Castle';
-
-
 
 const index = require('./routes/index');
 app.use('/', index);
